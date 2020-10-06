b94c0bff398e51710b243d581d3213b1c281ce30
edevars <e.devars.dev@gmail.com>
2020-10-06 10:14:57 -0500

    :sparkles: Preliminar posts views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.views import generic
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
-class PostList(generic.ListView):
+
+class PostList(ListView):
+    # Only shows the published posts, in status 1
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
+
+class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
