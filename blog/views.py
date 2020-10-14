1541aadc243015868d633815ad75e0d419ba27ce
edevars <e.devars.dev@gmail.com>
2020-10-13 20:51:06 -0500

    :recycle: Refactoring blog template folder

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ from .models import Post
 class PostList(ListView):
     # Only shows the published posts, in status 1
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
